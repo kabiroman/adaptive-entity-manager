738539d53a6d01850eaba89ef18ece601e447e90
Ruslan Kabirov <kabirovruslan@gmail.com>
2025-05-29 00:56:39 +0300

    Added Config class

---
+++ src/Config.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kabiroman\AEM;
+
+class Config implements ConfigInterface
+{
+    public function __construct(
+        public readonly string $entityFolder = __DIR__."/../../../src/Entity",
+        public readonly string $entityNamespace = 'App\\Entity\\',
+        public readonly string $cacheFolder = __DIR__ . "/../../../var/cache",
+    ) {
+    }
+
+    public function getEntityFolder(): string
+    {
+        return $this->entityFolder;
+    }
+
+    public function getEntityNamespace(): string
+    {
+        return $this->entityNamespace;
+    }
+
+    public function getCacheFolder(): string
+    {
+        return $this->cacheFolder;
+    }
+}
