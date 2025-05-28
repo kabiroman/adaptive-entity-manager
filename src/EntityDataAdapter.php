0ca649d330787dcf6957173d6cfde3c33eb63cd8
Ruslan Kabirov <kabirovruslan@gmail.com>
2025-05-29 00:22:06 +0300

    Added EntityDataAdapter interface

---
+++ src/EntityDataAdapter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kabiroman\AEM;
+
+interface EntityDataAdapter
+{
+    /**
+     * @return array The identifier array.
+     */
+    public function insert(array $row): array;
+
+    /**
+     * @return mixed|void
+     */
+    public function update(array $identifier, array $row);
+
+    /**
+     * @return mixed|void
+     */
+    public function delete(array $identifier);
+
+    /**
+     * @return array The entity data row.
+     */
+    public function refresh(array $identifier): array;
+
+    /**
+     * @return array|null The entity data row.
+     */
+    public function loadById(array $identifier): array|null;
+
+    /**
+     * @return array The collection of entity data row.
+     */
+    public function loadAll(
+        array $criteria = [],
+        array|null $orderBy = null,
+        int|null $limit = null,
+        int|null $offset = null,
+    ): array;
+}
