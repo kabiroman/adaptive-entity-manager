0b3d2d2e99e5ad584bf45fc16f42269eac4bd6b9
Ruslan Kabirov <kabirovruslan@gmail.com>
2025-05-29 01:01:29 +0300

    Added constants

---
+++ src/Constant/FetchModeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kabiroman\AEM\Constant;
+
+enum FetchModeEnum: string
+{
+    case EAGER = 'EAGER';
+    case LAZY = 'LAZY';
+}
---
+++ src/Constant/FieldTypeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kabiroman\AEM\Constant;
+
+enum FieldTypeEnum: string
+{
+    case Integer = 'int';
+    case String = 'string';
+    case Boolean = 'bool';
+    case DateTime = 'datetime';
+    case Float = 'float';
+}
