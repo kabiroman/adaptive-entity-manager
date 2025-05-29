93bc53432ae1dfcdf3cda69862b54f4884a80239
Ruslan Kabirov <51466769+kabiroman@users.noreply.github.com>
2025-05-29 23:02:52 +0300

    Fix Config.php
    Fixed: Corrected folder paths in Config.php to resolve issues with incorrect directory references.

--- src/Config.php
+++ src/Config.php
@@ -5,9 +5,9 @@ namespace Kabiroman\AEM;
 class Config implements ConfigInterface
 {
     public function __construct(
-        public readonly string $entityFolder = __DIR__."/../../../src/Entity",
+        public readonly string $entityFolder = __DIR__."/../../../../src/Entity",
         public readonly string $entityNamespace = 'App\\Entity\\',
-        public readonly string $cacheFolder = __DIR__ . "/../../../var/cache",
+        public readonly string $cacheFolder = __DIR__ . "/../../../../var/cache",
     ) {
     }
 
